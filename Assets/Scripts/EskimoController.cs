a939db2df39213133130010aaf138a14d207c8ce
Gustav <gustav.grube05@gmail.com>
2019-11-16 23:42:44 +0100

    dwda

--- Assets/Scripts/EskimoController.cs
+++ Assets/Scripts/EskimoController.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class EskimoController : MonoBehaviour
 {
+    public Rigidbody2D EskimoRB;
+    public int Speed;
+    public int HorizontalSpeed;
+    public Vector2 Movement;
+    public Vector2 ForwardDirection;
+    public Vector2 BackDirection;
+    public Vector2 RightDirection;
+    public Vector2 LeftDirection;
+    public static int Score;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,27 @@ public class EskimoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            Movement = Vector2.up * Speed;
+            //BoatRB.AddForce(Speed * ForwardDirection);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Movement = Vector2.down * Speed;
+            //             BoatRB.AddForce(Speed * BackDirection);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Movement = Vector2.right * Speed;
+            //BoatRB.AddForce(HorizontalSpeed * RightDirection);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Movement = Vector2.left * Speed;
+            //BoatRB.AddForce(HorizontalSpeed * LeftDirection);
+        }
+        transform.Translate(Movement * Time.deltaTime);
+        Movement = Movement * 0.99f;
     }
 }
