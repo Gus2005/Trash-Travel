60584bccf4000194142ad64c949f49a012151dae
Gustav <gustav.grube05@gmail.com>
2019-11-17 01:38:38 +0100

    dwd

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+
+    public Transform sparkle;
+
+    // Start is called before the first frame update
+    [System.Obsolete]
+    void Start()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+
+
+    }
+    [System.Obsolete]
+    void OnTriggerEnter2D()
+    {
+        sparkle.GetComponent<ParticleSystem>().enableEmission = true;
+        StartCoroutine(stopSparkles());
+    }
+    [System.Obsolete]
+    IEnumerator stopSparkles()
+    {
+        yield return new WaitForSeconds(.4f);
+
+        sparkle.GetComponent<ParticleSystem>().enableEmission = false;
+    }
+
+}
